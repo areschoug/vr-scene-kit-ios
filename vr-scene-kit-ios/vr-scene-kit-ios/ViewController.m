4781468605aec6b282c267dbc5108a1ad91116e7
andreas.areschoug@gmail.com <areschoug@areschougs-MacBook-Pro.local>
2014-11-14 22:33:29 +0100

    Default view controller setup

--- vr-scene-kit-ios/vr-scene-kit-ios/ViewController.m
+++ vr-scene-kit-ios/vr-scene-kit-ios/ViewController.m
@@ -9,8 +9,14 @@
 #import "ViewController.h"
 
 #import "VRView.h"
+#import "VRMotionManager.h"
+
+#import "VRView+Debug.h"
 
 @interface ViewController ()
+
+@property VRView *vrView;
+@property VRMotionManager *motionManager;
 
 @end
 
@@ -18,12 +24,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //create scene
+    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.dae"];
+    
+    //vr view
+    _vrView = [[VRView alloc] initWithFrame:self.view.bounds];
+    [_vrView setScene:scene];
+    [self.view addSubview:_vrView];
+    
+#ifdef DEBUG
+    [_vrView enableDebugMode];
+#endif
+    
+    //motion manger
+    _motionManager = [VRMotionManager motionManager];
+    _motionManager.cameraNode = (SCNNode *)_vrView.camera;
+    
+    //ship
+    SCNNode *ship = [scene.rootNode childNodeWithName:@"ship" recursively:NO];
+    ship.position = SCNVector3Make(0, 0, -10);
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [_motionManager start];
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [_motionManager stop];
 }
 
 @end
